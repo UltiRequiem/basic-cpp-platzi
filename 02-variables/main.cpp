4fd12c377e1f6b677ca8a4410e08f3ad17372635
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-09 18:35:19 -0500

    :art: Update

---
+++ 02-variables/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age = 14;
+  edad = edad 9 + 1;
+  cout << age;
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Basic C++
+# Basic C++ Course
 
 ![Cover](./utils/cover.png)
